fdb75002de41d4c573f42455fd613387257e8931
Vigneshwar <bvignesh2025@gmail.com>
2025-02-26 11:00:28 +0530

    Initial commit of Sorting Visualizer

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template ,jsonify
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    arr = [random.randint(10, 500) for _ in range(100)]  # 50 random numbers
+    return render_template('index.html', arr=arr)
+
+@app.route('/get_array')
+def get_array():
+    arr = [random.randint(10, 500) for _ in range(100)]  # 50 random numbers
+    return jsonify(arr)
+
+if __name__ == '__main__':
+    app.run(debug=True)
